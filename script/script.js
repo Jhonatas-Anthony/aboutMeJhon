f0d9cb70d21177196b50ad328546536d1cc2c2f2
jhonatas-anthony <jhonatasanthony6@gmail.com>
2023-04-03 08:40:24 -0300

    deleção e mudanças

--- script/script.js
+++ script/script.js
@@ -1,23 +1,34 @@
-let data = new Date()
-let dia = data.getDate().toString().padStart(2, '0')
-let mes = (data.getMonth() + 1).toString().padStart(2, '0')
-let ano = data.getFullYear().toString()
-let dataString = `${dia}/${mes}/${ano}`
-let dataHTML = document.querySelector('span#data')
-dataHTML.innerHTML = dataString
-/////////////////////////////////////////////////////////////
+
 let sections = document.querySelectorAll('main section');
 
 sections.forEach(function (section) {
     let title = section.querySelector('section h2')
     let content = section.querySelector('.collapse');
     let clicked = section
+    
 
     title.addEventListener('click', function () {
         content.classList.toggle('show')
         clicked.classList.toggle('expand')
     });
 });
+////////////////////////////////////////////////////////////////
+let skillsImg = document.querySelectorAll('img.skills')
+let skillText = document.querySelector('h3#skillTitle')
+
+skillsImg.forEach((imagem) => {
+    imagem.addEventListener("mouseover", () => {
+        skillText.innerHTML = imagem.getAttribute("data-titulo");
+    });
+    
+    imagem.addEventListener("mouseout", () => {
+        skillText.innerHTML = "Clique ou passe o mouse";
+    });
+  });
+
+ScrollReveal().reveal('#main', { delay: 100 });
+ScrollReveal().reveal('#skills-container', { delay: 100 });
+ScrollReveal().reveal('section', {delay: 100});
 
 
 
